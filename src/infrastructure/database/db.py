ecfa1a8cb1fd12ba2b4f1a8a99086582e8ffd404
Abdelrahman Abdeen <abdelrahman.abden@sociumtech.com>
2025-03-22 13:22:46 +0300

    small fix

--- src/infrastructure/database/db.py
+++ src/infrastructure/database/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData, create_engine
 
-DATABASE_URL = DATABASE_URL = 'postgresql://abood:abood123@localhost:5432/db_project'
+DATABASE_URL = 'postgresql://abood:abood123@localhost:5432/db_project'
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
